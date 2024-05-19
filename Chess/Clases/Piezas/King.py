4c99df9e50559e2458c0b773dcb0ecfc3d6b301f
Agustin Trachta <agustrachta@gmail.com>
2024-05-19 11:44:40 -0400

    cambios y test

--- Chess/Clases/Piezas/King.py
+++
@@ -1,9 +0,0 @@
-import pygame
-import os
-from Clases.Constantes import *
-
-class King:
-    def __init__(self, Square, image, color, type, row, col):
-        
-        Black_King = pygame.transform.scale(pygame.image.load(os.path.join("Clases/Imagenes", "bK.png")), (Square, Square))
-        White_King = pygame.transform.scale(pygame.image.load(os.path.join("Clases/Imagenes", "wK.png")), (Square, Square))
