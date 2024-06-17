5dc3efc24d87940450d43b3061b270d964cbb5d1
Agustin Trachta <agustrachta@gmail.com>
2024-06-17 11:35:29 -0400

    correccion de tests

--- Chess1.0.1/Chess/chess_game/board.py
+++ Chess1.0.1/Chess/chess_game/board.py
@@ -20,32 +20,32 @@ class Board:
         for row in range(self.Rows):
             self.Board.append([0 for _ in range(self.Cols)])
             for col in range(self.Cols):
-#                if row == 1:
-#                    self.Board[row][col] = Pawn(self.Square, Black_Pawn, Black, "Pawn", row, col)
-#                if row == 6:
-#                    self.Board[row][col] = Pawn(self.Square, White_Pawn, White, "Pawn", row, col)
+                if row == 1:
+                    self.Board[row][col] = Pawn(self.Square, Black_Pawn, Black, "Pawn", row, col)
+                if row == 6:
+                    self.Board[row][col] = Pawn(self.Square, White_Pawn, White, "Pawn", row, col)
 
                 if row == 0:
-#                    if col == 0 or col == 7:
-#                        self.Board[row][col] = Rook(self.Square, Black_Rook, Black, "Rook", row, col)
+                    if col == 0 or col == 7:
+                        self.Board[row][col] = Rook(self.Square, Black_Rook, Black, "Rook", row, col)
                     if col == 1 or col == 6:
                        self.Board[row][col] = Knight(self.Square, Black_Knight, Black, "Knight", row, col)
-#                    if col == 2 or col == 5:
-#                        self.Board[row][col] = Bishop(self.Square, Black_Bishop, Black, "Bishop", row, col)
-#                    if col == 3:
-#                        self.Board[row][col] = Queen(self.Square, Black_Queen, Black, "Queen", row, col)
+                    if col == 2 or col == 5:
+                        self.Board[row][col] = Bishop(self.Square, Black_Bishop, Black, "Bishop", row, col)
+                    if col == 3:
+                        self.Board[row][col] = Queen(self.Square, Black_Queen, Black, "Queen", row, col)
                     if col == 4:
                         self.Board[row][col] = King(self.Square, Black_King, Black, "King", row, col)
 
                 if row == 7:
-#                    if col == 0 or col == 7:
-#                        self.Board[row][col] = Rook(self.Square, White_Rook, White, "Rook", row, col)
+                    if col == 0 or col == 7:
+                        self.Board[row][col] = Rook(self.Square, White_Rook, White, "Rook", row, col)
                     if col == 1 or col == 6:
                         self.Board[row][col] = Knight(self.Square, White_Knight, White, "Knight", row, col)
-#                    if col == 2 or col == 5:
-#                        self.Board[row][col] = Bishop(self.Square, White_Bishop, White, "Bishop", row, col)
-#                    if col == 3:
-#                        self.Board[row][col] = Queen(self.Square, White_Queen, White, "Queen", row, col)
+                    if col == 2 or col == 5:
+                        self.Board[row][col] = Bishop(self.Square, White_Bishop, White, "Bishop", row, col)
+                    if col == 3:
+                        self.Board[row][col] = Queen(self.Square, White_Queen, White, "Queen", row, col)
                     if col == 4:
                         self.Board[row][col] = King(self.Square, White_King, White, "King", row, col)
 
